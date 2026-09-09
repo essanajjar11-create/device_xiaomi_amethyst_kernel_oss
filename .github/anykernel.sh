9ac40c733d3c28965cd688fcbde25edbc3cad1da
essanajjar11-create <essa.najjar11@gmail.com>
2026-09-09 20:02:58 +0300

    packaging: switch to WildKernels AnyKernel3 gki-2.0 backend

--- .github/anykernel.sh
+++ .github/anykernel.sh
@@ -1,28 +1,63 @@
 ### AnyKernel3 Ramdisk Mod Script
+## WildKernels GKI 2.0 configuration
 ## osm0sis @ xda-developers
 
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Sina Kernel for Xiaomi Redmi Note 14 Pro+ (Amethyst)
+kernel.string=Sina-Amethyst Kernel 6.1.68
 do.devicecheck=0
 do.modules=0
-do.systemless=1
+do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
+do.check_boot_version=0
+device.name1=
+device.name2=
+device.name3=
+device.name4=
+device.name5=
+supported.versions=
+supported.patchlevels=
+supported.vendorpatchlevels=
+keycheck.timeout=10
 '; } # end properties
 
 ### AnyKernel install
-# boot shell variables
-BLOCK=boot;
-IS_SLOT_DEVICE=auto;
-RAMDISK_COMPRESSION=auto;
-PATCH_VBMETA_FLAG=auto;
+## boot shell variables
+block=boot
+is_slot_device=auto
+ramdisk_compression=auto
+patch_vbmeta_flag=auto
+no_magisk_check=1
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
-. tools/ak3-core.sh;
+. tools/ak3-core.sh
+
+# GKI check
+kernel_version=$(cat /proc/version | awk -F '-' '{print $1}' | awk '{print $3}')
+case $kernel_version in
+    5.10*) ksu_supported=true ;;
+    5.15*) ksu_supported=true ;;
+    6.1*) ksu_supported=true ;;
+    6.6*) ksu_supported=true ;;
+    6.12*) ksu_supported=true ;;
+    *) ksu_supported=false ;;
+esac
+
+ui_print " " "  -> GKI Supported: $ksu_supported"
+$ksu_supported || abort "  -> Non-GKI device, abort."
 
 # boot install
-split_boot;
-flash_boot;
-## end boot install
+split_boot
+
+if [ -f "$SPLITIMG/ramdisk.cpio" ]; then
+    unpack_ramdisk
+    write_boot
+else
+    flash_boot
+fi
+
+ui_print " "
+ui_print "  -> Sina-Amethyst Kernel 6.1.68 installed successfully!"
+ui_print " "
